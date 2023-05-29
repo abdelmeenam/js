cf2f907a5d9bca6e52111357bbc66a350145e4dc
abdelmeenam <abdelmeenam@gmail.com>
2023-05-29 08:43:40 +0300

    Events

--- 6. The Document Object Model/DOM4.js
+++ 6. The Document Object Model/DOM4.js
@@ -1,32 +1,34 @@
-// Events
-const button = document.querySelector('button');
-
-
-// show data of dom
 const itmes = document.querySelectorAll('li');
+const button = document.querySelector('button');
+const ul = document.querySelector('ul');
+
+
+// Remove  & ADD from DOM
 // itmes.forEach(item => {
 //     item.addEventListener('click', e => {
 //         //console.log(e);
 //         //console.log(e.target);
 //         //console.log(item);
-//         e.target.style.textDecoration = 'line-through';
+//         //e.target.style.textDecoration = 'line-through';
+//         //e.target.remove();
+//         //e.stopPropagation();
+//         item.remove();
 //     });
 // });
-
-
-// Remove from DOM
-itmes.forEach(item => {
-    item.addEventListener('click', e => {
-        item.remove();
-    });
-});
-
-// Add data to DOM
-const ul = document.querySelector('ul');
 button.addEventListener('click', () => {
     //ul.innerHTML += '<li>something new to do</li>';
     const li = document.createElement('li');
     li.textContent = 'something new to do';
-    //ul.append(li);
     ul.prepend(li);
 });
+
+
+// Event bubling
+ul.addEventListener('click', e => {
+    //console.log('even in UL')
+    //console.log(e.target);
+    if (e.target.tagName === 'LI') {
+        e.target.remove();
+    }
+});
+
