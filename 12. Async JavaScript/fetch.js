f339aa32bda81fe914d5fe8b099021bcf0c4304a
abdelmeenam <abdelmeenam@gmail.com>
2023-06-08 20:01:11 +0300

    async & await

--- 12. Async JavaScript/fetch.js
+++ 12. Async JavaScript/fetch.js
@@ -20,10 +20,16 @@
 const getTodos = async () => {
 
     const response = await fetch('todos/lolo.json');
-    const data = await response.json();
 
+    if (response.status !== 200) {
+        throw new Error('cannot fetch the data');
+    }
+
+    const data = await response.json();
     return data;
 }
+
 getTodos()
-    .then(data => console.log('resolved:', data));
+    .then(data => console.log('resolved:', data))
+    .catch(err => console.log('rejected:', err.message));
 
