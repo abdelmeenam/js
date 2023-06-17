0dcc43140e0356d90de6b19d7ff5f41e92960434
abdelmeenam <abdelmeenam@gmail.com>
2023-06-17 18:28:35 +0300

    13. Project - Weather App

--- 13. Project - Weather App/scripts/forecast.js
+++ 13. Project - Weather App/scripts/forecast.js
@@ -3,36 +3,25 @@ const key = 'dwSiVNLesTWVJLMnsGqS8MzYBT0PlvB6';
 // get weather information
 const getWeather = async (id) => {
 
-    // base url
     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
 
     // wait to fetch data
     const response = await fetch(base + query);
     const data = await response.json();
-
     return data[0];
 };
 
-// get ciry information
+// get city information
 const getCity = async (city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
 
-    // query string parameters
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     // wait to fetch data
     const response = await fetch(base + query);
     const data = await response.json();
-
-    //returns a promise
     return data[0];
 };
 
-
-getCity('benha').then(data => {
-    return getWeather(data.Key);
-}).then(data => {
-    console.log(data);
-}).catch(err => console.log(err));
 
