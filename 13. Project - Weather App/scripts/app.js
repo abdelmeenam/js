7e7e28458a2d3c84313ab33691636e74526a68a6
abdelmeenam <abdelmeenam@gmail.com>
2023-06-20 11:30:34 +0300

    13. Project - Weather App

--- 13. Project - Weather App/scripts/app.js
+++ 13. Project - Weather App/scripts/app.js
@@ -3,6 +3,27 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 
 
+const updateUI = (data) => {
+
+    // destructure properties
+    const { cityDets, weather } = data;
+
+    // update details template
+    details.innerHTML = `
+            <h5 class="my-3">${cityDets.EnglishName}</h5>
+            <div class="my-3">${weather.WeatherText}</div>
+            <div class="display-4 my-4">
+                <span>${weather.Temperature.Metric.Value}</span>
+                <span>&deg;C</span>
+            </div>
+        `;
+
+    // remove d-none class if present
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+}
+
 const updateCity = async (city) => {
 
     const cityDets = await getCity(city);
@@ -23,8 +44,6 @@ cityForm.addEventListener('submit', e => {
 
     // update the ui with new city
     updateCity(city)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(err => console.log(err));
-    // .then(data => updateUI(data))
-    // .catch(err => console.log(err));
 });
