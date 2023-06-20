a583c4c3a1efb5f9d6982753cc61e9d8e7d12e10
abdelmeenam <abdelmeenam@gmail.com>
2023-06-20 12:17:40 +0300

    14. Local Storage

---
+++ 14. Local Storage/index.js
@@ -0,0 +1,18 @@
+// Store data in local storage
+localStorage.setItem('name', 'soso');
+localStorage.setItem('age', 50);
+
+
+// Get data from local storage
+let name = localStorage.getItem('name');
+let age = localStorage.getItem('age');
+
+
+// Updating data
+localStorage.setItem('name', 'roro');
+localStorage.age = '11';
+
+
+// Deleting data from local storage
+localStorage.removeItem('name');
+localStorage.clear();
---
+++ 14. Local Storage/index1.js
@@ -0,0 +1,13 @@
+// store more complex data in local Storage
+const todos = [
+    { text: 'play mariokart', author: 'shaun' },
+    { text: 'buy some milk', author: 'mario' },
+    { text: 'buy some bread', author: 'luigi' }
+]
+
+//console.log(JSON.stringify(todos));
+localStorage.setItem('todos', JSON.stringify(todos));
+
+
+const stored = localStorage.getItem('todos');
+console.log(JSON.parse(stored));
