51cf2ead30a9fb52ec1203e653392b1cee8c20d6
abdelmeenam <abdelmeenam@gmail.com>
2023-06-20 12:46:44 +0300

    18. More ES6 Features

---
+++ 18. More ES6 Features/index.js
@@ -0,0 +1,16 @@
+// rest parameter
+const double = (...nums) => {
+}
+
+
+// spread syntax
+const people = ['ali', 'roro', 'soso'];
+const members = ['mone', ...people];
+
+// speard synatx with objects
+const person = {
+    name: 'ali',
+    age: 20
+}
+
+const personColne = { ...person, location: 'benha' }
